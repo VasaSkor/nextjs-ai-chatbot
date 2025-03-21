38dbda7bcdfd99a993db7cddc878dbb18fb92a18
VasaSkor <106305964+vasaskor@users.noreply.github.com>
2025-03-21 19:08:17 +0300

    feat: test commit 9

--- app/(chat)/api/character/route.ts
+++ app/(chat)/api/character/route.ts
@@ -2,6 +2,19 @@ import { NextRequest, NextResponse } from "next/server";
 import { updateRegularPrompt } from "@/lib/ai/prompts";
 import { changeSystemPromptInChat } from "@/lib/db/queries";
 
-export async function GET() {
-    return NextResponse.json({ message: "API is working" });
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+
+    console.log(body['chatId']);
+    if (!body['chatId']) {
+        await updateRegularPrompt(body['newPrompt']);
+        return NextResponse.json({});
+    }
+
+    await changeSystemPromptInChat({
+        chatId: body['chatId'],
+        newSystemPrompt: body['newPrompt']
+    });
+
+    return NextResponse.json({});
 }
